c551dc48f2d753df261a9ad690205edbc7363dc7
Andrew <you@example.com>
2025-11-29 22:54:58 -0600

    Images to blog, featured post on front page.

---
+++ src/scripts/andrewTheObject.js
@@ -0,0 +1,14 @@
+const vieau = {
+    firstName: "Andrew",
+    middleName: "Douglas",
+    lastName: "Vieau",
+    occupation: "Solution Consultant",
+    location: "Minneapolis, MN",
+    beard: true,
+    hobbies: ["hiking", "video games", "reading", "traveling", ],
+    languages: ["English", "French", "Spanish"],
+    skills: ["Sitecore", "JavaScript", "C#", "React", "Nextjs",],
+    pet: null,
+    homeTown: "Buffalo, MN",
+    
+}
